566bd643bb0118856b7f581dfd4921be58dcdd2c
Numan Şenlik <nmnsnlk@gmail.com>
2024-01-18 23:57:10 +0300

    create chatsidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+import BackgroundImage from "../public/whatsapp-bg.png";
+import ChatSidebar from "@/modules/ChatSidebar";
 export default function Home() {
   return (
     <main className="flex h-screen overflow-hidden">
-      <h1>Whatsapp clone</h1>
+      <div className="bg-white-500 w-1/3 overflow-y-auto">
+        <ChatSidebar />
+      </div>
+      <div className="w-full overflow-y-auto flex items-center justify-center bg-blue-50">
+        <Image src={BackgroundImage} alt="Logo" width={3000} height={3000} />
+      </div>
     </main>
   );
 }
---
+++ modules/ChatSidebar.tsx
@@ -0,0 +1,8 @@
+const ChatSidebar: React.FC = () => {
+  return (
+    <div>
+      <h1>ChatSidebar</h1>
+    </div>
+  );
+};
+export default ChatSidebar;
