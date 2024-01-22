b1a14822c3990a398b046e369182a8556b9d4432
Numan Şenlik <nmnsnlk@gmail.com>
2024-01-22 22:43:51 +0300

    Create MessageBody

---
+++ components/MainScreen/MessageBody.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+
+const MessageBody: React.FC = () => {
+  return (
+    <div
+      className={`overflow-y-auto flex flex-col min-h-screen justify-end bg-[url('https://user-images.githubusercontent.com/15075759/28719144-86dc0f70-73b1-11e7-911d-60d70fcded21.png')]`}
+    >
+      Hello
+    </div>
+  );
+};
+
+export default MessageBody;
--- modules/MainScreen.tsx
+++ modules/MainScreen.tsx
@@ -1,10 +1,12 @@
 import MainScreenHeader from "@/components/MainScreen/MainScreenHeader";
+import MessageBody from "@/components/MainScreen/MessageBody";
 import React from "react";
 
 const MainScreen: React.FC = () => {
   return (
     <div className="w-full flex flex-col ">
       <MainScreenHeader />
+      <MessageBody />
     </div>
   );
 };
